f3949f1b24b92b76279304edb629c4c200bef681
mikeacosta <mike@acosta.net>
2022-12-16 07:39:10 -0800

    logging

--- src/main/java/net/postcore/apilist/cli/ApiRetriever.java
+++ src/main/java/net/postcore/apilist/cli/ApiRetriever.java
@@ -1,23 +1,26 @@
 package net.postcore.apilist.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ApiRetriever {
+    private static final Logger LOG = LoggerFactory.getLogger(ApiRetriever.class);
 
     public static void main(String[] args) {
-        System.out.println("ApiRetriever started!");
+        LOG.info("ApiRetriever starting");
         if (args.length == 0) {
-            System.out.println("First argument should be API category.");
+            LOG.warn("First argument should be API category.");
             return;
         }
 
         try {
             retrieveApis(args[0]);
         } catch (Exception e) {
-            System.out.println("Unexpected error");
-            e.printStackTrace();
+            LOG.error("Unexpected error", e);
         }
     }
 
     private static void retrieveApis(String category) {
-        System.out.println("Retrieving APIs for category \"" + category + "\"");
+        LOG.info("Retrieving APIs for category \"{}\"", category);
     }
 }
