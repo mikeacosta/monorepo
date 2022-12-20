dbd765584d6d90d6b101ebc7830602b71ce24a90
mikeacosta <mike@acosta.net>
2022-12-20 08:14:44 -0800

    unit testing

--- src/main/java/net/postcore/apilist/cli/service/PublicApi.java
+++ src/main/java/net/postcore/apilist/cli/service/PublicApi.java
@@ -1,4 +1,12 @@
 package net.postcore.apilist.cli.service;
 
-public record PublicApi(String API, String Description, String Auth, String HTTPS, String Cors, String Link, String Category) {
+import org.apache.commons.validator.routines.UrlValidator;
+
+public record PublicApi(String API, String Description, String Auth, Boolean HTTPS, String Cors, String Link, String Category) {
+
+    public boolean isLinkValid() {
+        String[] schemes = {"http","https"};
+        UrlValidator urlValidator = new UrlValidator(schemes);
+        return urlValidator.isValid(Link);
+    }
 }
