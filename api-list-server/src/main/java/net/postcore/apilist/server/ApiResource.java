3626b7c94a7525a68360805c0d575262c2afef9f
mikeacosta <mike@acosta.net>
2022-12-27 20:28:51 -0800

    create JAX-RS resource

---
+++ api-list-server/src/main/java/net/postcore/apilist/server/ApiResource.java
@@ -0,0 +1,33 @@
+package net.postcore.apilist.server;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import net.postcore.apilist.domain.ApiRecord;
+import net.postcore.apilist.repository.ApiRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Comparator.comparing;
+
+@Path("apis")
+public class ApiResource {
+    private static final Logger LOG = LoggerFactory.getLogger(ApiResource.class);
+
+    private ApiRepository apiRepository;
+
+    public ApiResource(ApiRepository apiRepository) {
+        this.apiRepository = apiRepository;
+    }
+
+    @GET
+    public String getApis() {
+        return apiRepository
+                .getAllApis()
+                .stream()
+                .map(ApiRecord::toString)
+                .collect(Collectors.joining(", "));
+    }
+}
