993a7a4ba0fba1fb4458bf6beeaa70e2172d7e8d
mikeacosta <mike@acosta.net>
2023-01-03 00:05:34 -0800

    api-list unified logging

--- api-list/api-list-server/src/main/java/net/postcore/apilist/server/ApiServer.java
+++ api-list/api-list-server/src/main/java/net/postcore/apilist/server/ApiServer.java
@@ -5,15 +5,23 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.net.URI;
+import java.util.logging.LogManager;
 
 public class ApiServer {
+
+    static {
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.install();
+    }
+
     private static final Logger LOG = LoggerFactory.getLogger(ApiServer.class);
     private static final String BASE_URI = "http://localhost:8080/";
 
     public static void main(String[] args) {
-        System.out.println("Starting HTTP server...");
+        LOG.info("Starting HTTP server...");
         ApiRepository apiRepository = ApiRepository.openApiRepository("./apis.db");
         ResourceConfig resourceConfig = new ResourceConfig().register(new ApiResource(apiRepository));
 
