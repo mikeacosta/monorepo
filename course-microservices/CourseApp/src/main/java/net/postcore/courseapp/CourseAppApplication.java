c921862e772655932faaca2eeb44c7751c37291f
mikeacosta <mike@acosta.net>
2023-01-05 09:03:35 -0800

    course JAR file

--- course-microservices/CourseApp/src/main/java/net/postcore/courseapp/CourseAppApplication.java
+++ course-microservices/CourseApp/src/main/java/net/postcore/courseapp/CourseAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseAppApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CourseAppApplication.class, args);
+        System.out.println("CourseApp is running...");
     }
 
 }
