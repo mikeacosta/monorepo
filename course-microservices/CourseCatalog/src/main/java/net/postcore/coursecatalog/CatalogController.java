a2592d67ac08f177584231bb5ce7e5093a98313c
mikeacosta <mike@acosta.net>
2023-01-17 07:09:07 -0800

    course app service discovery

--- course-microservices/CourseCatalog/src/main/java/net/postcore/coursecatalog/CatalogController.java
+++ course-microservices/CourseCatalog/src/main/java/net/postcore/coursecatalog/CatalogController.java
@@ -1,5 +1,8 @@
 package net.postcore.coursecatalog;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -7,12 +10,16 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CatalogController {
 
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    private InstanceInfo instanceInfo;
+
     @RequestMapping("/")
     public String getCatalogHome() {
         String courseAppMessage = "";
-        String courseAppUrl = "http://localhost:8080";
         RestTemplate restTemplate = new RestTemplate();
-        courseAppMessage = restTemplate.getForObject(courseAppUrl, String.class);
+        courseAppMessage = restTemplate.getForObject(getCourseAppUrl(), String.class);
 
         return "Welcome to Course Catalog" + '\n' + courseAppMessage;
     }
@@ -20,9 +27,8 @@ public class CatalogController {
     @RequestMapping("/catalog")
     public String getCatalog() {
         String courses = "";
-        String courseAppUrl = "http://localhost:8080/courses";
         RestTemplate restTemplate = new RestTemplate();
-        courses = restTemplate.getForObject(courseAppUrl, String.class);
+        courses = restTemplate.getForObject(getCourseAppUrl() + "/courses", String.class);
 
         return "Our courses are: " + '\n' + courses;
     }
@@ -30,10 +36,16 @@ public class CatalogController {
     @RequestMapping("/firstcourse")
     public String getCatalogCourse() {
         Course course = null;
-        String courseAppUrl = "http://localhost:8080/courses/1";
         RestTemplate restTemplate = new RestTemplate();
-        course = restTemplate.getForObject(courseAppUrl, Course.class);
+        course = restTemplate.getForObject(getCourseAppUrl() + "/courses/1", Course.class);
 
         return "Our first course is: " + (course != null ? course.getCourseName() : " not found");
+    }
+
+    private String getCourseAppUrl() {
+        if (instanceInfo == null)
+            instanceInfo = eurekaClient.getNextServerFromEureka("course-app", false);
+
+        return instanceInfo.getHomePageUrl();
     }
 }
