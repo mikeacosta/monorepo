286168bcd1dcaa0f1189db4c1078d5d8df32fc38
mikeacosta <mike@acosta.net>
2023-01-17 17:22:33 -0800

    return breweries JSOn

--- breweries/breweries-server/src/main/java/net/postcore/breweries/server/BreweryResource.java
+++ breweries/breweries-server/src/main/java/net/postcore/breweries/server/BreweryResource.java
@@ -1,13 +1,18 @@
 package net.postcore.breweries.server;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import net.postcore.breweries.domain.Brewery;
 import net.postcore.breweries.repository.BreweryRepository;
+import net.postcore.breweries.repository.RepositoryException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.List;
 
 @Path("/breweries")
 public class BreweryResource {
@@ -21,11 +26,17 @@ public class BreweryResource {
     }
 
     @GET
-    public String getBreweries() {
-        return breweryRepository
-                .getAllBreweries()
-                .stream()
-                .map(Brewery::toString)
-                .collect(Collectors.joining(", "));
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Brewery> getBreweries() {
+        try {
+            return breweryRepository
+                    .getAllBreweries()
+                    .stream()
+                    .sorted(Comparator.comparing(Brewery::name))
+                    .toList();
+        } catch (RepositoryException e) {
+            LOG.error("Could not retrieve breweries from the database", e);
+            throw  new NotFoundException();
+        }
     }
 }
