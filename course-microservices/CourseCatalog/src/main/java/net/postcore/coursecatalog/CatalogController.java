8173142d20d8458281203aeee41dc0e0b4e7d225
mikeacosta <mike@acosta.net>
2023-01-19 17:29:24 -0800

    Resilience4J circuit breaker

--- course-microservices/CourseCatalog/src/main/java/net/postcore/coursecatalog/CatalogController.java
+++ course-microservices/CourseCatalog/src/main/java/net/postcore/coursecatalog/CatalogController.java
@@ -3,6 +3,8 @@ package net.postcore.coursecatalog;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -13,39 +15,41 @@ public class CatalogController {
     @Autowired
     private EurekaClient eurekaClient;
 
-    private InstanceInfo instanceInfo;
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
 
     @RequestMapping("/")
     public String getCatalogHome() {
-        String courseAppMessage = "";
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
+
+        String url = getCourseAppUrl();
         RestTemplate restTemplate = new RestTemplate();
-        courseAppMessage = restTemplate.getForObject(getCourseAppUrl(), String.class);
+        String courseAppMessage = circuitBreaker.run(() -> restTemplate.getForObject(url, String.class),
+                throwable -> displayDefaultHome());
 
         return "Welcome to Course Catalog" + '\n' + courseAppMessage;
     }
 
+    private String displayDefaultHome() {
+        return("Welcome to Course Catalog " + " Please try again later");
+    }
+
     @RequestMapping("/catalog")
     public String getCatalog() {
-        String courses = "";
         RestTemplate restTemplate = new RestTemplate();
-        courses = restTemplate.getForObject(getCourseAppUrl() + "/courses", String.class);
-
+        String courses = restTemplate.getForObject(getCourseAppUrl() + "/courses", String.class);
         return "Our courses are: " + '\n' + courses;
     }
 
     @RequestMapping("/firstcourse")
     public String getCatalogCourse() {
-        Course course = null;
         RestTemplate restTemplate = new RestTemplate();
-        course = restTemplate.getForObject(getCourseAppUrl() + "/courses/1", Course.class);
-
+        Course course = restTemplate.getForObject(getCourseAppUrl() + "/courses/1", Course.class);
         return "Our first course is: " + (course != null ? course.getCourseName() : " not found");
     }
 
     private String getCourseAppUrl() {
-        if (instanceInfo == null)
-            instanceInfo = eurekaClient.getNextServerFromEureka("course-app", false);
-
+        InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("course-app", false);
         return instanceInfo.getHomePageUrl();
     }
 }
