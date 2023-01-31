4f7124357ef7512df42f2d178a2e5e7904693fab
mikeacosta <mike@acosta.net>
2023-01-31 09:18:07 -0800

    integrate order service with config server

--- course-microservices/OrderService/src/main/java/net/postcore/orderservice/OrderController.java
+++ course-microservices/OrderService/src/main/java/net/postcore/orderservice/OrderController.java
@@ -1,13 +1,20 @@
 package net.postcore.orderservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
 
+    @Value("${organization.name}")
+    private String organizationName;
+
+    @Value("${service.welcome.message}")
+    private String serviceMessage;
+
     @RequestMapping("/")
     public String getOrder() {
-        return("Hello from Order module");
+        return(organizationName + " *** " + serviceMessage);
     }
 }
