75a185dc3dbe4f04f1780a109379cbd72fb50707
mikeacosta <mike@acosta.net>
2023-02-01 23:46:16 -0800

    initialize critter care

---
+++ critter-care/src/test/java/net/postcore/crittercare/CritterCareApplicationTests.java
@@ -0,0 +1,13 @@
+package net.postcore.crittercare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CritterCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
