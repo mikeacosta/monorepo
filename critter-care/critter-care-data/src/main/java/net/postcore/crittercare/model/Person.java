2363701788e8fa81dc790e876d09165b8d0bd34d
mikeacosta <mike@acosta.net>
2023-02-07 09:54:06 -0800

    add base entity, closes #10

--- critter-care/critter-care-data/src/main/java/net/postcore/crittercare/model/Person.java
+++ critter-care/critter-care-data/src/main/java/net/postcore/crittercare/model/Person.java
@@ -1,6 +1,6 @@
 package net.postcore.crittercare.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- critter-care/critter-care-data/src/main/java/net/postcore/crittercare/model/PetType.java
+++ critter-care/critter-care-data/src/main/java/net/postcore/crittercare/model/PetType.java
@@ -1,6 +1,6 @@
 package net.postcore.crittercare.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
