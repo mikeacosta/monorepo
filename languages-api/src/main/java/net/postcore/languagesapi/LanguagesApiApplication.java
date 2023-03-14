31cba6aa76b1f3a75d43e784223c4db8d065c80b
mikeacosta <mike@acosta.net>
2023-03-14 08:28:55 -0700

    initialize languages API

---
+++ languages-api/src/main/java/net/postcore/languagesapi/LanguagesApiApplication.java
@@ -0,0 +1,14 @@
+package net.postcore.languagesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguagesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguagesApiApplication.class, args);
+        System.out.println("languages-api started...");
+    }
+
+}
