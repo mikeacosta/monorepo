2b8ff21a5935fccf430ad92f40b99d071be9703b
mikeacosta <mike@acosta.net>
2023-03-23 12:04:45 -0700

    return API child resources

--- CityInfo/CityInfo.API/Models/CitiesDataStore.cs
+++ CityInfo/CityInfo.API/Models/CitiesDataStore.cs
@@ -14,19 +14,64 @@ public class CitiesDataStore
             {
                 Id = 1,
                 Name = "London",
-                Description = "capital of the United Kingdom"
+                Description = "capital of the United Kingdom",
+                PointsOfInterest = new List<PointOfInterestDto>()
+                {
+                    new PointOfInterestDto()
+                    {
+                        Id = 1, 
+                        Name = "Borough Market", 
+                        Description = "One of London's oldest food markets"
+                    },
+                    new PointOfInterestDto()
+                    {
+                        Id = 2, 
+                        Name = "Tower of London", 
+                        Description = "Historic castle with over 1,000 years of history"
+                    }
+                }
             },
             new CityDto()
             {
                 Id = 2,
                 Name = "Honolulu",
-                Description = "Aloha"
+                Description = "Aloha",
+                PointsOfInterest = new List<PointOfInterestDto>()
+                {
+                    new PointOfInterestDto()
+                    {
+                        Id = 3, 
+                        Name = "Waikiki Beach", 
+                        Description = "This iconic landmark in Waikiki is one of Hawaii's most famous beaches"
+                    },
+                    new PointOfInterestDto()
+                    {
+                        Id = 4, 
+                        Name = "Diamond Head", 
+                        Description = "Popular volcanic tuff cone on Oahu"
+                    }
+                }
             },
             new CityDto()
             {
                 Id = 3,
                 Name = "Oakland",
-                Description = "There's no there there"
+                Description = "There's no there there",
+                PointsOfInterest = new List<PointOfInterestDto>()
+                {
+                    new PointOfInterestDto()
+                    {
+                        Id = 5, 
+                        Name = "Jack London Square", 
+                        Description = "Entertainment and business destination on the waterfront"
+                    },
+                    new PointOfInterestDto()
+                    {
+                        Id = 6, 
+                        Name = "Lake Merritt", 
+                        Description = "Large tidal lagoon in the center of Oakland"
+                    }
+                }
             }
         };
     }
