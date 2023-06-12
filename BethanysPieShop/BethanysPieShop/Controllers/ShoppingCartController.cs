e95f6f2e6d5dcb3087ef79ffcd333e519226b87f
mikeacosta <mike@acosta.net>
2023-06-12 06:31:30 -0700

    shopping cart navigation

--- BethanysPieShop/BethanysPieShop/Controllers/ShoppingCartController.cs
+++ BethanysPieShop/BethanysPieShop/Controllers/ShoppingCartController.cs
@@ -1,5 +1,6 @@
 using BethanysPieShop.Models;
 using BethanysPieShop.Repositories;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers;
@@ -13,5 +14,37 @@ public class ShoppingCartController : Controller
     {
         _pieRepository = pieRepository;
         _shoppingCart = shoppingCart;
+
+    }
+    public ViewResult Index()
+    {
+        var items = _shoppingCart.GetShoppingCartItems();
+        _shoppingCart.ShoppingCartItems = items;
+
+        var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, _shoppingCart.GetShoppingCartTotal());
+
+        return View(shoppingCartViewModel);
+    }
+
+    public RedirectToActionResult AddToShoppingCart(int pieId)
+    {
+        var selectedPie = _pieRepository.AllPies.FirstOrDefault(p => p.PieId == pieId);
+
+        if (selectedPie != null)
+        {
+            _shoppingCart.AddToCart(selectedPie);
+        }
+        return RedirectToAction("Index");
+    }
+
+    public RedirectToActionResult RemoveFromShoppingCart(int pieId)
+    {
+        var selectedPie = _pieRepository.AllPies.FirstOrDefault(p => p.PieId == pieId);
+
+        if (selectedPie != null)
+        {
+            _shoppingCart.RemoveFromCart(selectedPie);
+        }
+        return RedirectToAction("Index");
     }
 }
