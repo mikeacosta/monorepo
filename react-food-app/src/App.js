6a365b7e8702c3523fe4786917fa08cab82bb38a
mikeacosta <mike@acosta.net>
2023-06-26 18:28:18 -0700

    initialize react-food-app

---
+++ react-food-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+const App = () => {
+  return (
+    React.createElement('div', {className: 'customClass', id: '123'}, 'blah blah blah')
+  )
+}
+
+export default App;
