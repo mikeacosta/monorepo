16d1b64471b5f79db40098fa4dbcaa11eaa74d4e
mikeacosta <mike@acosta.net>
2023-07-10 12:32:10 -0700

    tic-tac-toe useState

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,30 +1,39 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Board = ()=> {
   return (
     <React.Fragment>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </React.Fragment>
   );
 }
 
-const Square = ()=> {
+const Square = () => {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
   return (
-    <button className="square">1</button>
+    <button 
+      className="square"
+      onClick={handleClick}>{value}</button>
   );
 }
 
