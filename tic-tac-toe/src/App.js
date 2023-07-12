7791ef148741f356173f350a87b1baab91b2c4cc
mikeacosta <mike@acosta.net>
2023-07-11 19:57:16 -0700

    lift state up to parent

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,38 +2,33 @@ import React from 'react';
 import { useState } from 'react';
 
 const Board = ()=> {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <React.Fragment>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </React.Fragment>
   );
 }
 
-const Square = () => {
-  const [value, setValue] = useState(null);
 
-  function handleClick() {
-    setValue('X');
-  }
-
+const Square = ({value}) => {
   return (
-    <button 
-      className="square"
-      onClick={handleClick}>{value}</button>
+    <button className="square">{value}</button>
   );
 }
 
