c43908e30b94104a35050ae8653140705a7a1328
mikeacosta <mike@acosta.net>
2023-08-08 06:23:21 -0700

    SpeakersToolbar component

--- conference/pages/index.js
+++ conference/pages/index.js
@@ -1,9 +1,16 @@
-import Speakers from "../src/components/Speakers";
+import { data } from "../SpeakerData";
+import Header from "../src/components/Header";
+import SpeakersToolbar from "../src/components/SpeakersToolbar";
+import SpeakersList from "../src/components/SpeakersList";
 
 const IndexPage = () => {
-    return (
-        <Speakers />
-    );
-}
+  return (
+    <div className="container-fluid">
+      <Header />
+      <SpeakersToolbar />
+      <SpeakersList data={data} />
+    </div>
+  )
+};
 
 export default IndexPage;
--- conference/src/components/Speakers.js
+++ conference/src/components/Speakers.js
@@ -1,14 +1,12 @@
 import { data } from "../../SpeakerData";
 import SpeakersList from "./SpeakersList";
-import Header from "./Header";
 
 function Speakers() {
-    return (
-        <div className="container-fluid">
-            <Header />
-            <SpeakersList data={data} />
-        </div>
-    )
+  return (
+    <div className="container-fluid">
+      <SpeakersList data={data} />
+    </div>
+  )
 }
 
 export default Speakers;
