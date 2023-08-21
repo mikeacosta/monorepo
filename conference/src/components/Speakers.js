5e01e5bf3a616d5f6822a42005e7bf3a016d320f
mikeacosta <mike@acosta.net>
2023-08-21 06:41:43 -0700

    lift theme state for sharing between components

--- conference/src/components/Speakers.js
+++ conference/src/components/Speakers.js
@@ -2,15 +2,22 @@ import { data } from "../../SpeakerData";
 import Header from "./Header";
 import SpeakersToolbar from "./SpeakersToolbar";
 import SpeakersList from "./SpeakersList";
+import { useState } from 'react';
 
 function Speakers() {
+  const [theme, setTheme] = useState("light");
+
   return (
-    <div className="container-fluid">
-      <Header />
-      <SpeakersToolbar />
+    <div className={
+      theme === "light" 
+        ? "container-fluid light" 
+        : "container-fluid dark"
+    }>
+      <Header theme={theme} />
+      <SpeakersToolbar theme={theme} setTheme={setTheme} />
       <SpeakersList data={data} />
     </div>
-  )
+  );
 }
 
 export default Speakers;
