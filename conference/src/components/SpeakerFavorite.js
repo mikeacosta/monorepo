a95001357aef1c62a880089b831bddaf9023bdfb
mikeacosta <mike@acosta.net>
2023-08-25 04:59:13 -0700

    update state from Speakers Card component

---
+++ conference/src/components/SpeakerFavorite.js
@@ -0,0 +1,15 @@
+function SpeakerFavorite({ favorite }) {
+  return (
+    <div className="action padB1">
+      <span>
+        <i className={
+            favorite === true ? "fa fa-star orange" : "fa fa-star-o orange"
+          }
+        />{" "}
+        Favorite{" "}
+      </span>
+    </div>
+  );
+}
+
+export default SpeakerFavorite;
