76760dfbade4f4fa909889f3237d81f92503b5a5
mikeacosta <mike@acosta.net>
2023-09-05 09:10:20 -0700

    combine useState and useEffect in custom hook

--- conference/src/components/SpeakersList.js
+++ conference/src/components/SpeakersList.js
@@ -1,44 +1,15 @@
-import { data } from "../../SpeakerData";
+
 import Speaker from "./Speaker";
-import { useState, useEffect } from "react";
 import ReactPlaceHolder from "react-placeholder";
+import useRequestSpeakers from "../hooks/useRequestSpeakers";
 
 function SpeakersList({showSessions}) {
-  const  [speakersData, setSpeakersData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasErrored, setHasErrored] = useState(false);
-  const [error, setError] = useState("");
 
-  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-  useEffect(() => {
-    async function delayFunc() {
-      try {
-        await delay(2000);
-        setIsLoading(false);
-        setSpeakersData(data);
-      } catch (e) {
-        setIsLoading(false);
-        setHasErrored(true);
-        setError(e);
-      }
-    }
-    delayFunc();
-  }, []);
-
-  function onFavoriteToggle(id) {
-    const speakersRecPrevious = speakersData.find(function (rec) {
-      return rec.id === id;
-    });
-    const speakerRecUpdated = {
-      ...speakersRecPrevious, favorite: !speakersRecPrevious.favorite,
-    };
-    const speakersDataNew = speakersData.map(function (rec) {
-      return rec.id === id ? speakerRecUpdated : rec;
-    });
-
-    setSpeakersData(speakersDataNew);
-  }
+  const {
+    speakersData, isLoading,
+    hasErrored, error,
+    onFavoriteToggle,
+  } = useRequestSpeakers(2000)
 
   if (hasErrored === true) {
     return (
