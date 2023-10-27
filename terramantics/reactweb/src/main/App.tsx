82614163f64b76cf0f92b439f3405c864eb93ed4
mikeacosta <mike@acosta.net>
2023-10-27 07:41:35 -0700

    add Bootstrap to Terramantics front-end

---
+++ terramantics/reactweb/src/main/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="Container">
+      <Header subtitle="Providing houses all over the world" />
+    </div>
+  );
+}
+
+export default App;
