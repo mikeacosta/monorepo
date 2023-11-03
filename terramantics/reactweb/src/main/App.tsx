708bd8d6d025b8b95a5278d01f48d3e8a4d61450
mikeacosta <mike@acosta.net>
2023-11-03 05:20:50 -0700

    set up routing

--- terramantics/reactweb/src/main/App.tsx
+++ terramantics/reactweb/src/main/App.tsx
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import HouseList from '../house/HouseList';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="Container">
-      <Header subtitle="Providing houses all over the world" />
-      <HouseList />
-    </div>
+    <BrowserRouter>
+      <div className="Container">
+        <Header subtitle="Providing houses all over the world" />
+        <Routes>
+          <Route path="/" element={<HouseList />}></Route>
+        </Routes>
+      </div>    
+    </BrowserRouter>
   );
 }
 
