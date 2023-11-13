94e36fe7fcdb6cf1819115d9505957e0654607db
mikeacosta <mike@acosta.net>
2023-11-13 12:12:55 -0700

    houses API CRUD endpoints

--- terramantics/api/Program.cs
+++ terramantics/api/Program.cs
@@ -1,5 +1,6 @@
 using api.Data;
 using api.Dtos;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -40,5 +41,27 @@ app.MapGet("/houses/{houseId:int}", async (int houseId, IHouseRepository repo) =
         return Results.Problem($"House with ID {houseId} not found.", statusCode: 404);
     return Results.Ok(house);
 }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
+
+app.MapPost("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) => 
+{
+    var newHouse = await repo.Add(dto);
+    return Results.Created($"/house/{newHouse.Id}", newHouse);
+}).ProducesValidationProblem().Produces<HouseDetailDto>(StatusCodes.Status201Created);
+
+app.MapPut("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) => 
+{
+    var updatedHouse = await repo.Update(dto);
+    if (await repo.Get(dto.Id) == null)
+        return Results.Problem($"House with Id {dto.Id} not found", statusCode: 404);    
+    return Results.Ok(updatedHouse);
+}).ProducesValidationProblem().ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
+
+app.MapDelete("/houses/{houseId:int}", async (int houseId, IHouseRepository repo) => 
+{
+    if (await repo.Get(houseId) == null)
+        return Results.Problem($"House with Id {houseId} not found", statusCode: 404);
+    await repo.Delete(houseId);
+    return Results.Ok();
+}).ProducesProblem(404).Produces(StatusCodes.Status200OK);
 
 app.Run();
