4e0f8078a78202cf61e78f59aa66172fd4be8cfd
mikeacosta <mike@acosta.net>
2023-11-14 04:31:38 -0800

    houses API database persistence

--- terramantics/api/Data/HouseRepository.cs
+++ terramantics/api/Data/HouseRepository.cs
@@ -7,6 +7,9 @@ public interface IHouseRepository
 {
     Task<List<HouseDto>> GetAll();
     Task<HouseDetailDto?> Get(int id);
+    Task<HouseDetailDto> Add(HouseDetailDto dto);
+    Task<HouseDetailDto> Update(HouseDetailDto dto);
+    Task Delete(int id);
 }
 
 public class HouseRepository : IHouseRepository
@@ -29,12 +32,54 @@ public class HouseRepository : IHouseRepository
         var entity = await _context.Houses.SingleOrDefaultAsync(h => h.Id == id);
         if (entity == null)
             return null;
+
+        return EntityToDetailDto(entity);
+    }
+
+    public async Task<HouseDetailDto> Add(HouseDetailDto dto)
+    {
+        var entity = new HouseEntity();
+        DtoToEntity(dto, entity);
+        _context.Houses.Add(entity);
+        await _context.SaveChangesAsync();
+        return EntityToDetailDto(entity);
+    }
+
+    public async Task<HouseDetailDto> Update(HouseDetailDto dto)
+    {
+        var entity = await _context.Houses.FindAsync(dto.Id);
         
-        return new HouseDetailDto(entity.Id,
-            entity.Address,
-            entity.Country,
-            entity.Description,
-            entity.Price,
-            entity.Photo);
+        if (entity == null)
+            throw new ArgumentException($"Trying to update house: entity with ID {dto.Id} not found.");
+        
+        DtoToEntity(dto, entity);
+        _context.Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return EntityToDetailDto(entity);
+    }
+
+    public async Task Delete(int id)
+    {
+        var entity = await _context.Houses.FindAsync(id);
+        
+        if (entity == null)
+            throw new ArgumentException($"Trying to delete house: entity with ID {id} not found.");
+        
+        _context.Houses.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+    
+    private static HouseDetailDto EntityToDetailDto(HouseEntity e)
+    {
+        return new HouseDetailDto(e.Id, e.Address, e.Country, e.Description, e.Price, e.Photo);
+    }
+
+    private static void DtoToEntity(HouseDetailDto dto, HouseEntity e)
+    {
+        e.Address = dto.Address;
+        e.Country = dto.Country;
+        e.Description = dto.Description;
+        e.Price = dto.Price;
+        e.Photo = dto.Photo;
     }
 }
