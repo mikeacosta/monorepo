a72dd9e9cf0914eabe04354456d02b98a101cc2a
mikeacosta <mike@acosta.net>
2023-12-04 09:36:12 -0700

    implement CRUD

--- HoopsPlayers/HoopsPlayersAPI/HoopsPlayersAPI/Controllers/HoopsPlayersController.cs
+++ HoopsPlayers/HoopsPlayersAPI/HoopsPlayersAPI/Controllers/HoopsPlayersController.cs
@@ -1,5 +1,8 @@
+using AutoMapper;
+using HoopsPlayersAPI.Entities;
 using HoopsPlayersAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HoopsPlayersAPI.Controllers;
 
@@ -7,19 +10,74 @@ namespace HoopsPlayersAPI.Controllers;
 [ApiController]
 public class HoopsPlayersController : ControllerBase
 {
-    [HttpGet]
-    public async Task<ActionResult<List<HoopsPlayer>>> GetHoopsPlayers()
+    private readonly DataContext _dataContext;
+    private readonly IMapper _mapper;
+    
+    public HoopsPlayersController(DataContext dataContext,
+        IMapper mapper)
     {
-        return await Task.Run(() => new List<HoopsPlayer>
-        {
-            new HoopsPlayer
-            {
-                Id = 1,
-                FirstName = "Stephen",
-                LastName = "Curry",
-                Team = "Warriors",
-                Country = "USA"
-            }
-        });
+        _dataContext = dataContext;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<HoopsPlayerDto>>> GetHoopsPlayers()
+    {
+        var players = await _dataContext.HoopsPlayers.OrderBy(p => p.Id).ToListAsync();
+        var dtos = _mapper.Map<IEnumerable<HoopsPlayerDto>>(players);
+        return Ok(dtos);
+    }
+    
+    [HttpGet("{id}", Name = "HoopsPlayerById")]
+    public async Task<ActionResult<HoopsPlayerDto>> GetHoopsPlayerById(int id)
+    {
+        var player = await _dataContext.HoopsPlayers.FindAsync(id);
+        if (player == null)
+            return NotFound();
+        
+        var dto = _mapper.Map<HoopsPlayerDto>(player);
+        return Ok(dto);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateHoopsPlayer([FromBody] CreateHoopsPlayerDto dto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest("Invalid model object");
+
+        var player = _mapper.Map<HoopsPlayer>(dto);
+
+        await _dataContext.AddAsync(player);
+        await _dataContext.SaveChangesAsync();
+        
+        var result = _mapper.Map<HoopsPlayerDto>(player);
+        return CreatedAtRoute("HoopsPlayerById", new { id = result.Id }, result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateHoopsPlayer(int id, [FromBody] UpdateHoopsPlayerDto dto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest("Invalid model object");
+
+        var playerEntity = await _dataContext.HoopsPlayers.FindAsync(id);
+        if (playerEntity is null)
+            return NotFound();
+
+        _mapper.Map(dto, playerEntity);
+        await _dataContext.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteHoopsPlayer(int id)
+    {
+        var playerEntity = await _dataContext.HoopsPlayers.FindAsync(id);
+        if (playerEntity is null)
+            return NotFound();
+
+        _dataContext.HoopsPlayers.Remove(playerEntity);
+        await _dataContext.SaveChangesAsync();
+        return NoContent();
     }
 }
--- HoopsPlayers/HoopsPlayersAPI/HoopsPlayersAPI/Profiles/MappingProfile.cs
+++ HoopsPlayers/HoopsPlayersAPI/HoopsPlayersAPI/Profiles/MappingProfile.cs
@@ -10,5 +10,6 @@ public class MappingProfile : Profile
     {
         CreateMap<HoopsPlayer, HoopsPlayerDto>();
         CreateMap<CreateHoopsPlayerDto, HoopsPlayer>();
+        CreateMap<UpdateHoopsPlayerDto, HoopsPlayer>();
     }
 }
