507abff798e3e6b8ec4906cd758505bbb8e4c700
mikeacosta <mike@acosta.net>
2023-12-13 19:05:12 -0800

    initialize sporting goods admin site

---
+++ CarvedRock.Admin/Data/Product.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarvedRock.Admin.Data;
+
+[Table("Products")]
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public bool IsActive { get; set; }
+}
