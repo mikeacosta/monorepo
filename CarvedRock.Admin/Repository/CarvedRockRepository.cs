eb4db884d09e73ca1ffa597da1948ee6d3058b67
mikeacosta <mike@acosta.net>
2023-12-15 22:35:10 -0800

    update read operations for related data

--- CarvedRock.Admin/Repository/CarvedRockRepository.cs
+++ CarvedRock.Admin/Repository/CarvedRockRepository.cs
@@ -15,13 +15,15 @@ public class CarvedRockRepository : ICarvedRockRepository
     public async Task<List<Product>> GetAllProductsAsync()
     {
         return await _context.Products
+            .Include(p => p.Category)
             .OrderBy(p => p.Id)
             .ToListAsync();
     }
 
     public async Task<Product?> GetProductByIdAsync(int productId)
     {
-        return await _context.Products            
+        return await _context.Products     
+            .Include(p => p.Category)
             .FirstOrDefaultAsync(m => m.Id == productId);
     }
 
