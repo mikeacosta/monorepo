f78d5f9df4eaab6e04b924829536270e24ef34c4
mikeacosta <mike@acosta.net>
2023-12-29 19:39:09 -0800

    AES encryption

---
+++ CryptoNET/AES/AesEncryption.cs
@@ -0,0 +1,22 @@
+using System.Security.Cryptography;
+
+namespace AES;
+
+public class AesEncryption
+{
+    public static byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+    {
+        using var aes = Aes.Create();
+        aes.Key = key;
+
+        return aes.EncryptCbc(dataToEncrypt, iv);
+    }
+
+    public static byte[] Decrypt(byte[] dataToDecrypt, byte[] key, byte[] iv)
+    {
+        using var aes = Aes.Create();
+        aes.Key = key;
+
+        return aes.DecryptCbc(dataToDecrypt, iv);
+    }
+}
---
+++ CryptoNET/AES/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.ReadLine();
+
+static void TestAesCBC()
+{
+    const string original = "Text to encrypt";
+}
