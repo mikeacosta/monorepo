31f3f9ffad824459cf46856dcf6317fa4b1897eb
mikeacosta <mike@acosta.net>
2024-01-03 09:22:25 -0700

    AES encryption using cipher block chaining

--- CryptoNET/AES/Program.cs
+++ CryptoNET/AES/Program.cs
@@ -1,9 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-Console.ReadLine();
+using System.Security.Cryptography;
+using System.Text;
+using AES;
 
-static void TestAesCBC()
+AES_CBC();
+
+static void AES_CBC()
 {
-    const string original = "Text to encrypt";
+    const string textToEncrypt = "If at first you don’t succeed; call it version 1.0";
+    
+    // 256-bit encryption key
+    var key = RandomNumberGenerator.GetBytes(32);
+    
+    // 128-bit initialization vector
+    var iv =  RandomNumberGenerator.GetBytes(16);
+    
+    // CipherMode = (default) cipher block chaining (CBC)
+    // PaddingMode = (default) PKCS7
+    
+    var encrypted = AesEncryption.Encrypt(Encoding.UTF8.GetBytes(textToEncrypt), key, iv);
+    var decrypted = AesEncryption.Decrypt(encrypted, key, iv);
+
+    var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+
+    Console.WriteLine("AES Encryption in .NET");
+    Console.WriteLine("------------------------------------");
+    Console.WriteLine();
+    Console.WriteLine("Plain Text = " + textToEncrypt);
+    Console.WriteLine("Encrypted Text = " + Convert.ToBase64String(encrypted));
+    Console.WriteLine("Decrypted Text = " + decryptedMessage);    
 }
