a6ed080a0fc7b2930e308100eee572dfa258446d
mikeacosta <mike@acosta.net>
2024-01-12 10:45:40 -0700

    cloudformation IAM service role

--- cloudformation/album-rank/command.sh
+++ cloudformation/album-rank/command.sh
@@ -1,3 +1,11 @@
+# create service role
+aws cloudformation create-stack \
+  --stack-name album-rank-service-role \
+  --template-body file://service-role.yaml \
+  --capabilities CAPABILITY_NAMED_IAM
+
+# create table
 aws cloudformation create-stack \
   --stack-name album-rank-table \
-  --template-body file://table.yaml
+  --template-body file://table.yaml \
+  --role-arn "arn:aws:iam::489967615225:role/AlbumRank-Cloudformation-ServiceRole"
