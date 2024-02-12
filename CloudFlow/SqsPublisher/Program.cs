07200da6ea0797ba05ec39fe249451f24a358029
mikeacosta <mike@acosta.net>
2024-02-12 00:12:42 -0700

    SQS msg publisher

--- CloudFlow/SqsPublisher/Program.cs
+++ CloudFlow/SqsPublisher/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("hey man!");
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated()
+{
+    Id = Guid.NewGuid(),
+    Email = "joeblow@gmail.com",
+    FullName = "Joe Blow",
+    DateOfBirth = new DateTime(1980, 1, 15),
+    GitHubUsername = "joeblow"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest()
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>()
+    {
+        {
+            "MessageType", new MessageAttributeValue() { DataType = "String", StringValue = nameof(CustomerCreated) }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine(response.ToString());
