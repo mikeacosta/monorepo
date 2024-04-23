58ea143163a11c6b9fa3ac8e7c1e0231b028d05d
mikeacosta <mike@acosta.net>
2024-04-22 21:17:40 -0700

    enable file upload

--- TownTalk/TownTalk.API/Controllers/FilesController.cs
+++ TownTalk/TownTalk.API/Controllers/FilesController.cs
@@ -29,4 +29,20 @@ public class FilesController : ControllerBase
         var bytes = System.IO.File.ReadAllBytes(pathToFile);
         return File(bytes, contentType, Path.GetFileName(pathToFile));
     }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateFile(IFormFile file)
+    {
+        if (file.Length == 0 || file.Length > 20971520 || file.ContentType != "application/pdf")
+            return BadRequest("No file or an invalid format has been input");
+
+        var path = Path.Combine(Directory.GetCurrentDirectory(), $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        return Ok("File uploaded successful");
+    }
 }
