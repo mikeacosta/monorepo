e9afb50eb27f34246457f467b7b0d43930c9d294
mikeacosta <mike@acosta.net>
2024-05-03 11:46:22 -0700

    XML to JSON Lambda

--- node-stuff/iso/src/app.js
+++ node-stuff/iso/src/app.js
@@ -1,12 +1,23 @@
+import { v4 as uuidv4 } from 'uuid';
+import xmlConverter from "xml-js";
+
 export const handler = async (event) => {
   // TODO implement
   console.log("request:", JSON.stringify(event, undefined, 2));
+  console.log("request body:", event.body);
 
-  console.log(event.body);
+  var now = new Date().toISOString();
+  var id = uuidv4();
+
+  var json = xmlConverter.xml2json(event.body, { compact: true, spaces: 2 })
+    .replace("#date#", now)
+    .replace("#uuid#", id);
+
+  console.log("converted:", json);
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hey!'),
+    body: json,
   };
   return response;
 };
