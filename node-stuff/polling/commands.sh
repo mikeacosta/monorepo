14a4ff1363c2553a1e2dd481f8c72c66f6b65503
mikeacosta <mike@acosta.net>
2024-05-08 20:55:17 -0700

    SQS Lambda SAM template

--- node-stuff/polling/commands.sh
+++ node-stuff/polling/commands.sh
@@ -1,7 +1,13 @@
-# create SQS
-aws cloudformation create-stack \
-  --stack-name sqs-stack \
-  --template-body file://queue.yaml
+# package cloudformation
+aws cloudformation package --s3-bucket a-bucket-01234 \
+  --template-file template.yaml \
+  --output-template-file gen/template-generated.yaml  
+
+ # deploy
+aws cloudformation deploy \
+  --template-file gen/template-generated.yaml \
+  --stack-name sqs-lambda-stack \
+  --capabilities CAPABILITY_IAM
 
 # delete SQS
-aws cloudformation delete-stack --stack-name sqs-stack  
+aws cloudformation delete-stack --stack-name sqs-lambda-stack  
