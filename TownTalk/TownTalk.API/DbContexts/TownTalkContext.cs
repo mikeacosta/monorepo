115d6a1c6fd5af04a24d4cb2e5cae830fabeee2f
mikeacosta <mike@acosta.net>
2024-05-15 19:35:47 -0700

    seed PostgreSQL database

--- TownTalk/TownTalk.API/DbContexts/TownTalkContext.cs
+++ TownTalk/TownTalk.API/DbContexts/TownTalkContext.cs
@@ -11,4 +11,66 @@ public class TownTalkContext : DbContext
     
     public DbSet<Town> Towns { get; set; } = null!;
     public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+            modelBuilder.Entity<Town>()
+                .HasData(
+               new Town("London")
+               {
+                   Id = 1,
+                   Description = "Capital of the United Kingdom"
+               },
+               new Town("Honolulu")
+               {
+                   Id = 2,
+                   Description = "Aloha"
+               },
+               new Town("Oakland")
+               {
+                   Id = 3,
+                   Description = "There's no there there"
+               });
+
+            modelBuilder.Entity<PointOfInterest>()
+             .HasData(
+               new PointOfInterest("Borough Market")
+               {
+                   Id = 1,
+                   TownId = 1,
+                   Description = "One of London's oldest food markets"
+               },
+               new PointOfInterest("Tower of London")
+               {
+                   Id = 2,
+                   TownId = 1,
+                   Description = "Historic castle with over 1,000 years of history"
+               },
+                 new PointOfInterest("Waikiki Beach")
+                 {
+                     Id = 3,
+                     TownId = 2,
+                     Description = "This iconic landmark in Waikiki is one of Hawaii's most famous beaches"
+                 },
+               new PointOfInterest("Diamond Head")
+               {
+                   Id = 4,
+                   TownId = 2,
+                   Description = "Popular volcanic tuff cone on Oahu"
+               },
+               new PointOfInterest("Jack London Square")
+               {
+                   Id = 5,
+                   TownId = 3,
+                   Description = "Entertainment and business destination on the waterfront"
+               },
+               new PointOfInterest("Lake Merritt")
+               {
+                   Id = 6,
+                   TownId = 3,
+                   Description = "Large tidal lagoon in the center of Oakland"
+               }
+               );
+            base.OnModelCreating(modelBuilder);        
+    }
 }
---
+++ TownTalk/TownTalk.API/Migrations/20240516023324_InitialDataSeed.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TownTalk.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Towns",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Capital of the United Kingdom", "London" },
+                    { 2, "Aloha", "Honolulu" },
+                    { 3, "There's no there there", "Oakland" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "Description", "Name", "TownId" },
+                values: new object[,]
+                {
+                    { 1, "One of London's oldest food markets", "Borough Market", 1 },
+                    { 2, "Historic castle with over 1,000 years of history", "Tower of London", 1 },
+                    { 3, "This iconic landmark in Waikiki is one of Hawaii's most famous beaches", "Waikiki Beach", 2 },
+                    { 4, "Popular volcanic tuff cone on Oahu", "Diamond Head", 2 },
+                    { 5, "Entertainment and business destination on the waterfront", "Jack London Square", 3 },
+                    { 6, "Large tidal lagoon in the center of Oakland", "Lake Merritt", 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Towns",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Towns",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Towns",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
