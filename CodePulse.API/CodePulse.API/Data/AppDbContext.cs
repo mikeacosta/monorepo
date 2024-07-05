53a14ccb059d8e7375820cf57846de8b897d3e99
mikeacosta <mike@acosta.net>
2024-07-05 06:29:55 -0700

    seed category data

--- CodePulse.API/CodePulse.API/Data/AppDbContext.cs
+++ CodePulse.API/CodePulse.API/Data/AppDbContext.cs
@@ -11,4 +11,39 @@ public class AppDbContext : DbContext
     
     public DbSet<BlogPost> BlogPosts { get; set; } = null!;
     public DbSet<Category> Categories { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Category>()
+            .HasData(
+                new Category
+                {
+                    Id = new Guid("c5eb4603-2e0b-457d-8ed4-da1f29fe4981"),
+                    Name = "HTML",
+                    UrlHandle = "html-blogs"
+                },
+                new Category
+                {
+                    Id = new Guid("8adaae23-11d1-4038-828d-6a4b5a800c52"),
+                    Name = "CSS",
+                    UrlHandle = "css-blogs"
+                },
+                new Category
+                {
+                    Id = new Guid("34540768-eda6-4962-b9e6-ac3976484c4a"),
+                    Name = "C#",
+                    UrlHandle = "csharp-blogs"
+                },
+                new Category
+                {
+                    Id = new Guid("15096235-0b6b-4a6e-9390-2f2a0a6f7b08"),
+                    Name = "TypeScript",
+                    UrlHandle = "ts-blogs"
+                } 
+            );
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
---
+++ CodePulse.API/CodePulse.API/Migrations/20240705132626_InitalDataSeed.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CodePulse.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalDataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name", "UrlHandle" },
+                values: new object[,]
+                {
+                    { new Guid("15096235-0b6b-4a6e-9390-2f2a0a6f7b08"), "TypeScript", "ts-blogs" },
+                    { new Guid("34540768-eda6-4962-b9e6-ac3976484c4a"), "C#", "csharp-blogs" },
+                    { new Guid("8adaae23-11d1-4038-828d-6a4b5a800c52"), "CSS", "css-blogs" },
+                    { new Guid("c5eb4603-2e0b-457d-8ed4-da1f29fe4981"), "HTML", "html-blogs" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("15096235-0b6b-4a6e-9390-2f2a0a6f7b08"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("34540768-eda6-4962-b9e6-ac3976484c4a"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("8adaae23-11d1-4038-828d-6a4b5a800c52"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("c5eb4603-2e0b-457d-8ed4-da1f29fe4981"));
+        }
+    }
+}
