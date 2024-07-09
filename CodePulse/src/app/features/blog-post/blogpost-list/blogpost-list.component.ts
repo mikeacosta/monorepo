f51d4e072297f576b50e0cba11078e5f76eb9bae
mikeacosta <mike@acosta.net>
2024-07-09 08:53:13 -0700

    blog posts list UI

--- CodePulse/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ CodePulse/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogPost } from '../models/blogpost.model';
+import { Observable } from 'rxjs';
+import { BlogPostService } from '../services/blog-post.service';
 
 @Component({
   selector: 'app-blogpost-list',
   templateUrl: './blogpost-list.component.html',
   styleUrls: ['./blogpost-list.component.css']
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit {
 
+  blogPosts$?: Observable<BlogPost[]>;
+
+  constructor(private blogPostService: BlogPostService) {
+
+  }
+
+  ngOnInit(): void {
+    this.blogPosts$ = this.blogPostService.getAllBlogPosts();
+  }
 }
--- CodePulse/src/app/features/blog-post/services/blog-post.service.ts
+++ CodePulse/src/app/features/blog-post/services/blog-post.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AddBlogpostRequest } from '../models/add-blogpost-request.model';
 import { environment } from 'src/environments/environment';
+import { BlogPost } from '../models/blogpost.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +15,8 @@ export class BlogPostService {
   createBlogPost(model: AddBlogpostRequest): Observable<void> {
     return this.http.post<void>(`${environment.apiBaseUrl}/api/blogposts`, model);
   }
+
+  getAllBlogPosts(): Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogposts`);
+  }  
 }
