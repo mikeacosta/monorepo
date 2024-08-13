abb8355bab91de08d51b2fb84c661824574ecead
mikeacosta <mike@acosta.net>
2024-08-13 12:24:05 -0700

    initialize members API

---
+++ node-stuff/members/commands.sh
@@ -0,0 +1,13 @@
+# package cloudformation
+aws cloudformation package --s3-bucket a-bucket-01234 \
+  --template-file template.yaml \
+  --output-template-file gen/template-generated.yaml
+
+ # deploy
+aws cloudformation deploy \
+  --template-file gen/template-generated.yaml \
+  --stack-name members-api-stack \
+  --capabilities CAPABILITY_IAM
+
+# clean up
+aws cloudformation delete-stack --stack-name members-api-stack   
