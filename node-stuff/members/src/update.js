3921e70b0888a15aa9f7322aa3f0901df0d289e5
mikeacosta <mike@acosta.net>
2024-08-23 12:22:23 -0700

    update members endpoint

---
+++ node-stuff/members/src/update.js
@@ -0,0 +1,58 @@
+import { UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+import { dbClient } from "./dbClient.js";
+
+export const handler = async (event) => {
+  try {
+    console.log("request:", JSON.stringify(event, undefined, 2));
+
+    let body = await updateMember(event);
+
+    console.log(body);
+
+    return {
+      statusCode: 200,
+      "headers": { "content-type": "application/json" },
+      body: JSON.stringify(body)
+    };
+  } catch (e) {
+    console.error(e);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Error occurred",
+        errorMsg: e.message,
+        errorStack: e.errorStack
+      })
+    };
+  }
+}
+
+const updateMember = async (event) => {
+  try {
+    const requestBody = JSON.parse(event.body);
+    const objKeys = Object.keys(requestBody);
+    console.log(`updateMember function. requestBody : "${requestBody}", objKeys: "${objKeys}"`);
+
+    const params = {
+      TableName: process.env.TABLE_NAME,
+      Key: marshall({ id: event.pathParameters.id }),
+      UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
+      ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
+        ...acc,
+        [`#key${index}`]: key,
+      }), {}),
+      ExpressionAttributeValues: marshall(objKeys.reduce((acc, key, index) => ({
+        ...acc,
+        [`:value${index}`]: requestBody[key],
+      }), {})),
+    };
+
+    const updateResult = await dbClient.send(new UpdateItemCommand(params));
+    return updateResult;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
