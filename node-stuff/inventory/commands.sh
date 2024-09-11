db86aed1e84dfd5ef6842daf63e120b53f59329a
mikeacosta <mike@acosta.net>
2024-09-10 18:44:23 -0700

    inventory lambda function

--- node-stuff/inventory/commands.sh
+++ node-stuff/inventory/commands.sh
@@ -1,2 +1,16 @@
+# package cloudformation
+aws cloudformation package --s3-bucket a-bucket-01234 \
+  --template-file template.yaml \
+  --output-template-file gen/template-generated.yaml
+
+ # deploy
+aws cloudformation deploy \
+  --template-file gen/template-generated.yaml \
+  --stack-name inventory-api-stack \
+  --capabilities CAPABILITY_IAM
+
 # write data
 aws dynamodb batch-write-item --request-items file://inventory-items.json 
+
+# clean up
+aws cloudformation delete-stack --stack-name inventory-api-stack  
---
+++ node-stuff/inventory/src/lambda.js
@@ -0,0 +1,63 @@
+import { ScanCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import { dbClient } from "./dbClient.js";
+
+export const handler = async (event) => {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+
+  try {
+    let body = {};
+
+    switch (event.httpMethod) {
+      case "GET":
+        if (event.pathParameters != null) {
+          //body = await getItem(event.pathParameters.sku);
+        } else {
+          body = await getAllItems();
+        }
+        break;
+
+      case "POST":
+        break;
+
+      default:
+        throw new Error(`Unsupported route: "${event.httpMethod}"`);
+    }
+
+    console.log(body);
+
+    return {
+      statusCode: 200,
+      "headers": { "content-type": "application/json" },
+      body: JSON.stringify(body)
+    };
+  } catch (e) {
+    console.error(e);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Error occurred",
+        errorMsg: e.message,
+        errorStack: e.errorStack
+      })
+    };
+  }
+};
+
+const getAllItems = async () => {
+  console.log("getAllItems");
+  try {
+    const params = {
+      TableName: process.env.TABLE_NAME
+    };
+
+    const { Items } = await dbClient.send(new ScanCommand(params));
+    console.log(Items);
+    return (Items) ? Items.map((item) => unmarshall(item)) : {};
+
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
