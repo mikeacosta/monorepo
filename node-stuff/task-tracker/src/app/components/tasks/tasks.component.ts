ea2f01dfd49c51909e50396f57c74d209b5fae4f
mikeacosta <mike@acosta.net>
2024-10-04 17:44:01 -0700

    submit add task

--- node-stuff/task-tracker/src/app/components/tasks/tasks.component.ts
+++ node-stuff/task-tracker/src/app/components/tasks/tasks.component.ts
@@ -30,4 +30,9 @@ export class TasksComponent implements OnInit {
     task.reminder = !task.reminder;
     this.taskService.updateTaskReminder(task).subscribe();
   } 
+
+  addTask(task: Task) {
+    console.log('hey');
+    // TODO: use service to add task
+  }  
 }
