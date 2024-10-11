66d8a17ce3791e54ede0f8e60b2dda70088a919b
mikeacosta <mike@acosta.net>
2024-10-10 20:04:30 -0700

    task-tracker UI service

--- node-stuff/task-tracker/src/app/components/add-task/add-task.component.ts
+++ node-stuff/task-tracker/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 import { Task } from 'src/app/Task';
 
 @Component({
@@ -6,11 +8,27 @@ import { Task } from 'src/app/Task';
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask: boolean | undefined;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {
+
+  }  
+  
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }  
 
   onSubmit() {
     if (!this.text) {
--- node-stuff/task-tracker/src/app/components/header/header.component.ts
+++ node-stuff/task-tracker/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,15 +10,23 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   title: string = "Task Tracker";
   showAddTask: boolean = false;
+  subscription: Subscription;
 
-  constructor() {}
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {
     
   }
 
-  toggleAddTask() {
-    console.log('toggle');
-    this.showAddTask = !this.showAddTask
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  } 
 }
