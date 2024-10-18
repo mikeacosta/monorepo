8b6b5962a644714d04a7f3388e46ec76a54c1895
mikeacosta <mike@acosta.net>
2024-10-18 16:04:18 -0700

    initialize Spring Boot 3 symposium

---
+++ symposium/src/main/java/net/postcore/symposium/ServletInitializer.java
@@ -0,0 +1,13 @@
+package net.postcore.symposium;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SymposiumApplication.class);
+	}
+
+}
---
+++ symposium/src/main/webapp/WEB-INF/jsp/greeting.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Greeting</title>
+        <link rel="shortcut icon" href="#" />
+    </head>
+    <body>
+        <h1>${message}</h1>
+    </body>
+</html>
