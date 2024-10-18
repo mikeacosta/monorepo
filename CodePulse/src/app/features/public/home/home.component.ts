6eb29c53b6e59f3cc86df5f031fb98a9c4063915
mikeacosta <mike@acosta.net>
2024-10-17 19:54:22 -0700

    home component blogs

--- CodePulse/src/app/features/public/home/home.component.ts
+++ CodePulse/src/app/features/public/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../../blog-post/models/blogpost.model';
+import { BlogPostService } from '../../blog-post/services/blog-post.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  blogs$?: Observable<BlogPost[]>;
+
+  constructor(private blogPostService: BlogPostService) {
+
+  }  
+  ngOnInit(): void {
+    this.blogs$ = this.blogPostService.getAllBlogPosts();
+  }
 
 }
