90ffa41592f16290125b440f4893f9b6df7f939b
mikeacosta <mike@acosta.net>
2024-10-23 19:34:10 -0700

    enable CORS

--- expense-tracker/src/main/java/net/postcore/expensetracker/controller/ExpenseController.java
+++ expense-tracker/src/main/java/net/postcore/expensetracker/controller/ExpenseController.java
@@ -5,12 +5,14 @@ import net.postcore.expensetracker.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1")
 public class ExpenseController {
