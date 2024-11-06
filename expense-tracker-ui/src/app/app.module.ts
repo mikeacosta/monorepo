55aa749b3d3fbd6f1032c3f0811115768f3085ae
mikeacosta <mike@acosta.net>
2024-11-05 20:20:09 -0800

    add-expense component

--- expense-tracker-ui/src/app/app.module.ts
+++ expense-tracker-ui/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ListExpensesComponent } from './components/list-expenses/list-expenses.component';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'expenses', component: ListExpensesComponent},
+  {path: 'addexpense', component: AddExpenseComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListExpensesComponent
+    ListExpensesComponent,
+    AddExpenseComponent
   ],
   imports: [
     BrowserModule,
