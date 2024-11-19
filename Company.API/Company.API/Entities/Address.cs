704a10b23d7086111c11ff20fc2f55151133710e
mikeacosta <mike@acosta.net>
2024-11-18 21:22:19 -0700

    initialize Company.API

---
+++ Company.API/Company.API/Entities/Address.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Company.API.Entities;
+
+public class Address
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]    
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string Address1 { get; set; }
+    
+    [MaxLength(100)]
+    public string Address2 { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string City { get; set; }
+    
+    [MaxLength(2)]
+    public string State { get; set; }
+    
+    [MaxLength(50)]
+    public string PostalCode { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string Country { get; set; }
+}
---
+++ Company.API/Company.API/Entities/Contact.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Company.API.Entities;
+
+public class Contact
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]    
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; }
+    
+    [MaxLength(100)]
+    public string Email { get; set; }
+
+    public int CompanyId { get; set; }
+}
