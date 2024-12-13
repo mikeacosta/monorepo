55a657761faaf93c4e5be1be75f4e286862be191
mikeacosta <mike@acosta.net>
2024-12-12 21:22:23 -0800

    output task data

--- easy-task/src/app/user/user.component.ts
+++ easy-task/src/app/user/user.component.ts
@@ -1,6 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
+interface User {
+  id: string;
+  avatar: string;
+  name: string;  
+}
 
 @Component({
   selector: 'app-user',
@@ -10,11 +14,7 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  };
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
