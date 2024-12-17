9fdead2537192af2630891cd69bc1172e4034bed
mikeacosta <mike@acosta.net>
2024-12-17 04:45:59 -0800

    add new task

--- easy-task/src/app/tasks/new-task/new-task.component.ts
+++ easy-task/src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,30 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
 
   onCancel() {
     this.cancel.emit();
+  }
+
+  onSubmit() {
+    let newTask: NewTaskData = {
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      dueDate: this.enteredDueDate
+    }
+    this.add.emit(newTask);
   }
 }
--- easy-task/src/app/tasks/tasks.component.ts
+++ easy-task/src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from './new-task/new-task.component';
 import { DUMMY_TASKS } from '../dummy-tasks'
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -20,7 +21,6 @@ export class TasksComponent {
   }
 
   onCompleteTask(id: string) {
-    console.log(id + " completed!");
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
@@ -31,4 +31,17 @@ export class TasksComponent {
   onCancelAddTask() {
     this.isAddingTask = false;
   }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate
+    });
+
+    this.isAddingTask = false;
+  }
 }
+ 
