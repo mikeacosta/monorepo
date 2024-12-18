a026055f89560338c40d5ead5ef615948d4f597f
mikeacosta <mike@acosta.net>
2024-12-18 06:38:41 -0800

    complete task with service

--- easy-task/src/app/tasks/tasks.component.ts
+++ easy-task/src/app/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from './new-task/new-task.component';
-import { type NewTaskData } from './task/task.model';
 import { TasksService } from './tasks.service';
 
 @Component({
@@ -19,10 +18,6 @@ export class TasksComponent {
 
   get selectedUserTasks() {
     return this.tasksService.getUserTasks(this.userId);
-  }
-
-  onCompleteTask(id: string) {
-    this.tasksService.removeTask(id);
   }
 
   onStartAddTask() {
