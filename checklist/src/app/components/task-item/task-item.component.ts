a3fac5b709a15977553e158f07764789fa86cec6
mikeacosta <mike@acosta.net>
2024-12-19 08:11:32 -0800

    Angular fontawesome

--- checklist/src/app/components/task-item/task-item.component.ts
+++ checklist/src/app/components/task-item/task-item.component.ts
@@ -1,12 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../Task';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
   @Input() task!: Task
+  faTimes = faTimes;
+
+  onIconClick(task: Task | undefined) {
+    console.log('icon clicked:' + task?.id);
+  }
 }
