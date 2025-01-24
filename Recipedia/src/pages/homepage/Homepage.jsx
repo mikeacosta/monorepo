b91b943234306d3389bc8164bff5bf89b72ba994
mikeacosta <mikeacosta510@gmail.com>
2025-01-23 20:35:14 -0800

    fetch recipes from API

--- Recipedia/src/pages/homepage/Homepage.jsx
+++ Recipedia/src/pages/homepage/Homepage.jsx
@@ -5,6 +5,17 @@ const Homepage = () => {
 
   const getDataFromSearchComponent = (data) => {
     console.log(data);
+
+    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=3083f903870640d8a4dec76ea6679b41&query=${data}`;
+
+    async function getRecipes() {
+      const response = await fetch(url);
+      const result = await response.json();
+
+      console.log(result)
+    }
+
+    getRecipes();
   };
 
   return (
