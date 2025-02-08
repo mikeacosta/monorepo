4ee9d39d0803f39c38efc039d82a5ec53fb5b3f4
mikeacosta <mikeacosta510@gmail.com>
2025-02-07 20:15:22 -0800

    sam build Python dependencies

--- reed-uk-etl/commands.sh
+++ reed-uk-etl/commands.sh
@@ -1,12 +1,12 @@
+# build
+sam build --template-file template.yaml
+
 # package cloudformation
-aws cloudformation package --s3-bucket a-bucket-01234 \
-  --template-file template.yaml \
-  --output-template-file gen/template-generated.yaml
+sam package --s3-bucket a-bucket-01234
 
  # deploy
-aws cloudformation deploy \
-  --template-file gen/template-generated.yaml \
-  --stack-name reed-uk-stack \
+sam deploy --stack-name reed-uk-stack \
+  --s3-bucket a-bucket-01234 \
   --capabilities CAPABILITY_IAM
 
 # clean up
