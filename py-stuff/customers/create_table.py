a693b649dd956410edb6171e4c9f76fd04a60173
mikeacosta <mikeacosta510@gmail.com>
2025-02-20 20:11:02 -0800

    create table

---
+++ py-stuff/customers/create_table.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  port=3307,
+  user="root",
+  password="Abc12345",
+  database="dev_db"
+)
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
