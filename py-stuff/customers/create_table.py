3f9c8917e2fdab5804e376b27508c2065c177038
mikeacosta <mikeacosta510@gmail.com>
2025-02-25 20:21:45 -0800

    primary key

--- py-stuff/customers/create_table.py
+++ py-stuff/customers/create_table.py
@@ -11,4 +11,5 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 # mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
-mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+# mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
